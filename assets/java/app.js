a53ea708abf4672f504f54a7f89886d8905c079c
White Lily <mmacalino03@gmail.com>
2018-07-22 01:21:58 -0400

    finished java

--- assets/java/app.js
+++ assets/java/app.js
@@ -23,7 +23,57 @@ database.ref().on("child_added", function(childSnap) {
     $("#trainTable > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + next + "</td><td>" + min + "</td></tr>");
 });
 
+
 database.ref().on("value", function(snapshot) {
    
+});
 
+$("#addTrainBtn").on("click", function() {
+
+    var trainName = $("#trainNameInput").val().trim();
+    var destination = $("#destinationInput").val().trim();
+    var firstTrain = $("#firstInput").val().trim();
+    var frequency = $("#frequencyInput").val().trim();
+
+    if (trainName == "") {
+        alert('Enter a train name.');
+        return false;
+    }
+    if (destination == "") {
+        alert('Enter a destination.');
+        return false;
+    }
+    if (firstTrain == "") {
+        alert('Enter a first train time.');
+        return false;
+    }
+    if (frequency == "") {
+        alert('Enter a frequency');
+        return false;
+    }
+
+    var firstTrainConverted = moment(firstTrain, "hh:mm").subtract("1, years");
+    var timeDifference = currentTime.diff(moment(firstTrainConverted), "minutes");
+    var remainder = timeDifference % frequency;
+    var minUntilTrain = frequency - remainder;
+    var nextTrainArrival = moment().add(minUntilTrain, "minutes").format("hh:mm a");
+
+    var newTrain = {
+        name: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        min: minUntilTrain,
+        next: nextTrainArrival
+    }
+
+    console.log(newTrain);
+    database.ref().push(newTrain);
+
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#firstInput").val("");
+    $("#frequencyInput").val("");
+
+    return false;
 });
